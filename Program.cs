222b88602ad209234f7e87f830a141e61b766eea
v-ramz <vr2025.pr@gmail.com>
2025-09-30 13:51:09 -0500

    [1.04] Create instance of Logger

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
+
+logger.Info("Program ended");

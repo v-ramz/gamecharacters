dd83b785076605c5a7eb52b2cb9b27673300e6f6
v-ramz <vr2025.pr@gmail.com>
2025-09-30 13:55:01 -0500

    [1.08] Deserialize mario json from file into List<Mario>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using System.Text.Json;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -6,6 +7,8 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+// deserialize mario json from file into List<Mario>
+string marioFileName = "mario.json";
+List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
 
 logger.Info("Program ended");

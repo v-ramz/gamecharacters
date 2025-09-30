300c175142d79885f19db5f773a23a69cec43ccd
v-ramz <vr2025.pr@gmail.com>
2025-09-30 13:55:52 -0500

    [1.09] Create menu of user choices

--- Program.cs
+++ Program.cs
@@ -11,4 +11,34 @@ logger.Info("Program started");
 string marioFileName = "mario.json";
 List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
 
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Display Mario Characters");
+  Console.WriteLine("2) Add Mario Character");
+  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("Enter to quit");
+
+  // input selection
+  string? choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+
+  if (choice == "1")
+  {
+    // Display Mario Characters
+  }
+  else if (choice == "2")
+  {
+    // Add Mario Character
+  }
+  else if (choice == "3")
+  {
+    // Remove Mario Character
+  } else if (string.IsNullOrEmpty(choice)) {
+    break;
+  } else {
+    logger.Info("Invalid choice");
+  }
+} while (true);
+
 logger.Info("Program ended");
